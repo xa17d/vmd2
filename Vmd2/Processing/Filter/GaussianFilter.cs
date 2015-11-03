c3c8257c40932526831c1c950160a74c42d22bef
Lucas Dobler <lucas@ldob.eu>
2015-11-03 12:46:55 +0100

    gaussian filter;

---
+++ Vmd2/Processing/Filter/GaussianFilter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vmd2.Processing.Filter
+{
+    class GaussianFilter
+    {
+        private double[,] filter;
+        private int rowCount;
+        private int columnCount;
+        private double divider;
+        public GaussianFilter(double[,] filter)
+        {
+            this.filter = filter;
+            this.rowCount = filter.GetLength(0);
+            this.columnCount = filter.GetLength(1);
+            this.divider = GetDivider();
+            this.Activated = true;
+
+            if (rowCount % 2 == 0 || columnCount % 2 == 0)
+            {
+                throw new ArgumentException("illegal filter size");
+            }
+        }
+
+        public bool Activated { set; get; }
+
+        public double FilterPixel(int x, int y, int z, Image3D image)
+        {
+            if(Activated)
+            {
+                double[,] area = image.GetArea(x, y, z, columnCount, rowCount);
+
+                double sum = 0;
+                for (int i = 0; i < columnCount; i++)
+                {
+                    for (int j = 0; j < rowCount; j++)
+                    {
+                        sum += area[j, i] * filter[j, i];
+                    }
+                }
+
+                return sum / divider;
+            }
+
+            return image[x,y,z];
+        }
+
+        private double GetDivider()
+        {
+            double sum = 0;
+
+            for (int i = 0; i < columnCount; i++)
+            {
+                for (int j = 0; j < rowCount; j++)
+                {
+                    sum += filter[j,i];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
--- Vmd2/Processing/Mapping/WindowingRenderer.cs
+++ Vmd2/Processing/Mapping/WindowingRenderer.cs
@@ -1,4 +1,6 @@
-﻿namespace Vmd2.Processing.Mapping
+﻿using Vmd2.Processing.Filter;
+
+namespace Vmd2.Processing.Mapping
 {
     class WindowingRenderer : RendererPixel
     {
@@ -8,16 +10,24 @@
             this.display = display;
             this.Window = window;
             this.Slice = 0;
+
+            // 3 x 3 Filter
+            //this.filter = new GaussianFilter(new double[3, 3] { { 1, 2, 1 }, { 2, 4, 2 }, { 1, 2, 1 } });
+
+            // 7 x 7 Filter
+            this.filter = new GaussianFilter(new double[7, 7] { { 1, 2, 3, 4, 3, 2, 1 }, { 2, 4, 6, 8, 6, 4, 2 }, { 3, 6, 9, 12, 9, 6, 3 }, { 4, 8, 12, 16, 12, 8, 4 }, { 3, 6, 9, 12, 9, 6, 3 }, { 2, 4, 6, 8, 6, 4, 2 }, { 1, 2, 3, 4, 3, 2, 1 } });
         }
 
         public int Slice { get; set; }
         public Windowing Window { get; }
         private Image3D image;
         private DisplayImage display;
+        private GaussianFilter filter;
 
         protected override void OnRenderPixel(int x, int y)
         {
-            var voxel = image[x, y, Slice];
+            //var voxel = image[x, y, Slice];
+            var voxel = filter.FilterPixel(x, y, Slice, image);
             display.SetPixel(x, y, Window.GetColor(voxel));
         }
 
